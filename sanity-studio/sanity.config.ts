5d8206a0f7ccbc3b4fd5180582f38645c10f2baa
Koray <koray_han_1903@hotmail.com>
2025-05-29 23:59:02 +0300

    test1

--- sanity-studio/sanity.config.ts
+++ sanity-studio/sanity.config.ts
@@ -1,16 +1,24 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
-import {schema} from './sanity/schemaTypes'
+import autoprefixer from 'autoprefixer'
+
+import {schemaTypes} from './sanity/schemaTypes'
 
 export default defineConfig({
   name: 'default',
   title: 'grappa-dev',
-
   projectId: 'hdvfijxo',
   dataset: 'production',
-
   plugins: [structureTool(), visionTool()],
-
-  schema,
+  schema: {
+    types: schemaTypes,
+  },
+  vite: {
+    css: {
+      postcss: {
+        plugins: [autoprefixer],
+      },
+    },
+  },
 })
--- sanity-studio/sanity/schemaTypes/index.ts
+++ sanity-studio/sanity/schemaTypes/index.ts
@@ -1,11 +1,7 @@
-import { type SchemaTypeDefinition } from "sanity";
+import {blockContentType} from './blockContentType'
+import {categoryType} from './categoryType'
+import {postType} from './postType'
+import {authorType} from './authorType'
+import {workType} from './workType'
 
-import { blockContentType } from "./blockContentType";
-import { categoryType } from "./categoryType";
-import { postType } from "./postType";
-import { authorType } from "./authorType";
-import { workType } from "./workType";
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, postType, authorType, workType],
-};
+export const schemaTypes = [blockContentType, categoryType, postType, authorType, workType]
