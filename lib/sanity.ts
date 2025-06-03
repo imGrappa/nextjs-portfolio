151248cce82b02ae0f7ce8c5667cc0d0bd4fa4c3
Koray <koray_han_1903@hotmail.com>
2025-06-04 00:19:57 +0300

    sanity fixed

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,17 +1,17 @@
 import { createClient } from "next-sanity";
 
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "development";
-export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-03-21";
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION!;
 
 export const client = createClient({
-  projectId: "2tz9zzxj",
-  dataset: "development",
-  apiVersion: "2024-03-21",
-  useCdn: true, // if you want to enable draft mode, set this to false
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: true, // set false if using draft mode
   perspective: "published",
 });
 
 export const urlFor = (source: any) => {
   return `https://cdn.sanity.io/images/${projectId}/${dataset}/${source}`;
-}; 
+};
